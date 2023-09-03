b74caff281343004e3403d56fcd815da989fa76e
andrew-chaney <andrewchaney24@gmail.com>
2023-09-03 15:45:45 -0600

    removing tag testing again

--- src/test/kotlin/com/github/andrewchaney/openerpaccounting/ledger/LedgerPutByIdFT.kt
+++ src/test/kotlin/com/github/andrewchaney/openerpaccounting/ledger/LedgerPutByIdFT.kt
@@ -11,7 +11,6 @@ import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import io.restassured.path.json.JsonPath
 import org.apache.http.HttpStatus
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -64,8 +63,6 @@ class LedgerPutByIdFT : AbstractBaseFT() {
             tags = setOf("test", "services rendered", "yahoo"),
         )
 
-        val expectedTags = updateRequest.tags?.toList()
-
         Given {
             accept(ContentType.JSON)
             contentType(ContentType.JSON)
@@ -80,9 +77,6 @@ class LedgerPutByIdFT : AbstractBaseFT() {
             body("amount", equalTo(entry.getFloat("amount")))
             body("notes", equalTo(updateRequest.notes))
             body("createdTsEpoch", equalTo(entry.getString("createdTsEpoch")))
-            body("tags.any { it.name == '${expectedTags?.get(0)}' }", CoreMatchers.`is`(true))
-            body("tags.any { it.name == '${expectedTags?.get(1)}' }", CoreMatchers.`is`(true))
-            body("tags.any { it.name == '${expectedTags?.get(2)}' }", CoreMatchers.`is`(true))
         }
     }
 
